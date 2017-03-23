2121d64779b41a355b263372d5f65ef5f585ae38
Erwan <erwan.iquel@gmail.com>
2017-03-23 16:09:33 +0100

    Bug: collision

--- asteroids.ml
+++ asteroids.ml
@@ -1,10 +1,10 @@
-
 open Graphics;;
 open List;; (* Ajout du module des listes pour plus d'ops sur les listes *)
 
+
 (* constantes et parametres *)
 let pi = 4.0 *. atan 1.0;;
-
+let couleurs = [| red; white; yellow; cyan; magenta; green; blue |];;
 
 (* dimension fenetre graphique *)
 let width = 1000;;
@@ -24,12 +24,17 @@ type vaisseau = {
 
 type asteroide = {
   rayon : int;
+  angle : float;
+  distance : int;
+  origine : position;
   position : position;
+  couleur : color;
   vitesse : int
 };;
 
 type projectile = {
   angle : float;
+  distance : int; (* Distance d'un projectile par rapport au vaisseau *)
   position : position;
   vitesse : int
 };;
@@ -46,15 +51,45 @@ type etat = {
 let degre_vers_radian a = (pi *. a) /. 180.0;;
 let d_v_r = degre_vers_radian;;
 
+(* Générateurs *)
+type 'a gen = unit -> 'a;;
 
+let lance (g : 'a gen) : 'a = g ();;
+
+let gen_un_cent = fun () -> 1 + (Random.int 50);;
+let gen_rayon = fun () -> 6 + (Random.int 85);;
+let gen_angle = fun () -> Random.int 360;;
+let gen_x = fun () -> ([| Random.int 401; 600 + (Random.int 401) |]).(Random.int 2);;
+let gen_y = fun () -> ([| Random.int 201; 400 + (Random.int 201) |]).(Random.int 2);;
+let gen_couleur = fun () -> couleurs.((Random.int 7));;
+let gen_vitesse = fun () -> 2 * (1 + Random.int 2);;
 
 (* --- initialisations etat --- *)
 (* A DEFINIR : generation positions, deplacements initiaux ... *)
+let rec faire_asteroide i =
+  let pos : position =
+    {
+        x = lance gen_x;
+        y = lance gen_y
+    }
+  in
+  let dist = lance gen_rayon in
+  if i = 0
+  then []
+  else {
+    rayon = dist;
+    distance = dist;
+    angle = float_of_int(lance gen_angle);
+    origine = pos;
+    position = pos;
+    couleur = lance gen_couleur;
+    vitesse = lance gen_vitesse
+  } :: faire_asteroide (i - 1)
 
 let init_etat () =
   {
       vaisseau = { angle = 90.0 };
-      asteroides = [];
+      asteroides = faire_asteroide (lance gen_un_cent);
       projectiles = []
   };; (* A REDEFINIR *)
 
@@ -87,61 +122,140 @@ let tir etat =
       asteroides = etat.asteroides;
       projectiles = {
         angle = etat.vaisseau.angle;
+        distance = 20;
         position = {
-          x = int_of_float(500.0 +. 20.0 *. cos (d_v_r etat.vaisseau.angle));
-          y = int_of_float(300.0 +. 20.0 *. sin (d_v_r etat.vaisseau.angle))
+          x = int_of_float(500.0 +. 10.0 *. cos (d_v_r etat.vaisseau.angle));
+          y = int_of_float(300.0 +. 10.0 *. sin (d_v_r etat.vaisseau.angle))
         };
-        vitesse = 1
+        vitesse = 6
       } :: etat.projectiles
   };;(* A REDEFINIR *)
 
 (* calcul de l'etat suivant, apres un pas de temps *)
-let rec mise_a_jour_projectiles l =
-  let maj a =
+let rec mise_a_jour_asteroides (l : asteroide list) =
+  let maj a x y dist =
     {
+      rayon = a.rayon;
       angle = a.angle;
-      position = {
-          x = a.position.x + int_of_float((1. *. (cos (d_v_r a.angle))));
-          y = a.position.y + int_of_float((1. *. (sin (d_v_r a.angle))))
+      distance = dist;
+      origine = {
+        x = x;
+        y = y
       };
-      vitesse = 1
+      position = {
+        x = int_of_float(float_of_int (x) +. float_of_int (dist) *. cos (d_v_r a.angle));
+        y = int_of_float(float_of_int (y) +. float_of_int (dist) *. sin (d_v_r a.angle))
+      };
+      couleur = a.couleur;
+      vitesse = a.vitesse
     }
   in
   match l with
   | [] -> []
-  | a::s -> (maj a)::mise_a_jour_projectiles(s);;
+  | a::s when a.position.x > (1000 + a.rayon) && (a.angle <= 90.0 || a.angle > 270.0) -> (maj a (0 - a.rayon) (a.position.y) a.rayon) :: (mise_a_jour_asteroides s)
+  | a::s when a.position.x < (0 - a.rayon) && (a.angle > 90.0 || a.angle <= 270.0) -> (maj a (1000 + a.rayon) (a.position.y) a.rayon) :: (mise_a_jour_asteroides s)
+  | a::s when a.position.y > (600 + a.rayon) && (a.angle >= 0.0 || a.angle < 180.0) -> (maj a (a.position.x) (0 - a.rayon) a.rayon) :: (mise_a_jour_asteroides s)
+  | a::s when a.position.y < (0 - a.rayon) && (a.angle < 360.0 || a.angle >= 180.0) -> (maj a (a.position.x) (600 + a.rayon) a.rayon) :: (mise_a_jour_asteroides s)
+  | a::s -> (maj a a.origine.x a.origine.y (a.distance + a.vitesse)) :: (mise_a_jour_asteroides s) ;;
+
+let maja = mise_a_jour_asteroides;;
+
+let rec mise_a_jour_projectiles (l : projectile list) =
+  let maj a =
+    {
+      angle = a.angle;
+      distance = a.distance + a.vitesse;
+      position = {
+          x = int_of_float(500.0 +. (float_of_int (a.distance + a.vitesse) *. (cos (d_v_r a.angle))));
+          y = int_of_float(300.0 +. (float_of_int (a.distance + a.vitesse) *. (sin (d_v_r a.angle))))
+      };
+      vitesse = a.vitesse
+    }
+  in
+  match l with
+  | [] -> []
+  | a::s when a.distance > 500 -> mise_a_jour_projectiles(s)
+  | a::s -> (maj a) :: (mise_a_jour_projectiles s);;
 
 let majp = mise_a_jour_projectiles;;
 
+(* Fonction qui test les collision avec les asteroides et les projectiles *)
+let collision_tir etat =
+  let res = {
+    vaisseau = etat.vaisseau;
+    asteroides = [];
+    projectiles = []
+  }
+  in
+  let col = ref false in
+
+  for k = 0 to 10 do
+    print_endline "Test";
+  done;
+
+  for i = 0 to ((length etat.asteroides) - 1) do
+    (*let a = nth etat.asteroides i in
+    let j = ref 0;
+
+    while not(col) && j < (length etat.projectiles) -1 do
+      let p = nth etat.projectiles j in
+
+      if(a.rayon * a.rayon < (a.origin.x - p.position.x) * (a.origin.x - p.position.x) + (a.origin.y - p.position.y) * (a.origin.y - p.position.y))
+      then res.projectiles = p :: res.projectiles; j := j + 1;
+
+      if (a.rayon * a.rayon >= (a.origin.x - p.position.x) * (a.origin.x - p.position.x) + (a.origin.y - p.position.y) * (a.origin.y - p.position.y))
+      then col := true
+    done;
+
+    if not(col)
+    then a :: res.asteroides;*)
+  done;
+  res
+
 let etat_suivant etat =
+  (*let res = collision_tir etat.asteroide etat.projectile in
+  let res2 = collision_vaisseau in*)
   {
     vaisseau = etat.vaisseau;
-    asteroides = etat.asteroides;
+    asteroides = maja etat.asteroides;
     projectiles = majp etat.projectiles
   };;
 (* --- affichages graphiques --- *)
 
 (* fonctions d'affichage du vaisseau, d'un asteroide, etc. *)
 
+(* Dessine le vaisseau avec sa nouvelle position (via ses coordonnées polaires) *)
+let affiche_vaisseau (vaisseau : vaisseau) =
+fill_poly [|
+  (int_of_float(500.0 +. 10.0 *. cos (d_v_r vaisseau.angle)), int_of_float(300.0 +. 10.0 *. sin (d_v_r vaisseau.angle)));
+  (int_of_float(500.0 +. 10.0 *. cos (d_v_r (vaisseau.angle -. 120.0))), int_of_float(300.0 +. 10.0 *. sin (d_v_r (vaisseau.angle -. 120.0))));
+  (int_of_float(500.0 +. 10.0 *. cos (d_v_r (vaisseau.angle +. 120.0))), int_of_float(300.0 +. 10.0 *. sin (d_v_r (vaisseau.angle +. 120.0))))
+|];
+set_color blue;
+fill_circle (int_of_float(500.0 +. 10.0 *. cos (d_v_r vaisseau.angle))) (int_of_float(300.0 +. 10.0 *. sin (d_v_r vaisseau.angle))) 2;;
+
+(* Dessine les projectiles *)
 let rec affiche_projectiles (projectiles : projectile list) =
   match projectiles with
   | [] -> ()
   | a::s -> fill_circle a.position.x a.position.y 2; affiche_projectiles s;;
 
+(* Dessine les asteroides *)
+let rec affiche_asteroides (asteroides : asteroide list) =
+  match asteroides with
+  | [] -> ()
+  | a::s -> fill_circle a.position.x a.position.y a.rayon; set_color a.couleur; affiche_asteroides s;;
+
+(* Dessine le fond noir et appelle les fonctions d'affichage *)
 let affiche_etat etat =
   set_color black;
   fill_rect 0 0 1000 600;
   set_color white;
   draw_circle 500 300 20;
-  fill_poly [|
-    (int_of_float(500.0 +. 20.0 *. cos (d_v_r etat.vaisseau.angle)), int_of_float(300.0 +. 20.0 *. sin (d_v_r etat.vaisseau.angle)));
-    (int_of_float(500.0 +. 20.0 *. cos (d_v_r (etat.vaisseau.angle -. 120.0))), int_of_float(300.0 +. 20.0 *. sin (d_v_r (etat.vaisseau.angle -. 120.0))));
-    (int_of_float(500.0 +. 20.0 *. cos (d_v_r (etat.vaisseau.angle +. 120.0))), int_of_float(300.0 +. 20.0 *. sin (d_v_r (etat.vaisseau.angle +. 120.0))))
-  |];
-  set_color blue;
-  fill_circle (int_of_float(500.0 +. 20.0 *. cos (d_v_r etat.vaisseau.angle))) (int_of_float(300.0 +. 20.0 *. sin (d_v_r etat.vaisseau.angle))) 2;
+  affiche_vaisseau etat.vaisseau;
   set_color yellow;
-  affiche_projectiles etat.projectiles;;
+  affiche_projectiles etat.projectiles;
+  affiche_asteroides etat.asteroides;;
 
 (* --- boucle d'interaction --- *)
 
